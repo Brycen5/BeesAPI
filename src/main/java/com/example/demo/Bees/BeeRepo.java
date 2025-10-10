17dca7d6f9cf4fda7616648fd43a9cae28533ec0
Brycen5 <bmparsons@uncg.edu>
2025-10-10 18:26:53 -0400

    completed repo

---
+++ src/main/java/com/example/demo/Bees/BeeRepo.java
@@ -0,0 +1,17 @@
+package com.example.demo.Bees;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.Bees.Bee.Student;
+
+@Repository
+public interface BeeRepo extends JpaRepository<Bee, Long> {
+    List<Bee> getBeeByName(String name);
+
+    @Query(value = "select * from bees s where s.name like %?1% ", nativeQuery = true)
+    List<Student> getBeesByDescription(String description);
+}
